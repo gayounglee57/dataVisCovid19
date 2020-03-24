ae86dcc9082a64e0a0e04b4e32119d9f716879ab
Gayoung Lee <gayoung.lee@foodstuffs.co.nz>
2020-03-24 14:01:57 +1300

    adds labels, checks for cumulative data and adds legend

--- src/MapChart.js
+++ src/MapChart.js
@@ -14,7 +14,7 @@ const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 const colorScale = scaleLinear()
-  .domain([1, 20000])
+  .domain([1, 5000])
   .range(["#ffedea", "#ff5233"]);
 
 const MapChart = (props) => {
@@ -26,34 +26,37 @@ const MapChart = (props) => {
   }, []);
 
   return (
-    <ComposableMap
-      projectionConfig={{
-        rotate: [-10, 0, 0],
-        scale: 147
-      }}
-    >
-      <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
-      <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-      {data.length > 0 && (
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            // geographies is array of all the countries from geojson url
-            geographies.map(geo => {
-              // data is array of all the rows in dataset
-              const countryArray = data.filter(d => d["Country/Region"].includes(geo.properties.NAME) && moment(d["Last Update"]).format("DDDD") === "0" + props.day);
-              const countrySum = countryArray.reduce((acc, current, index) => acc + parseInt(countryArray[index].Confirmed), 0);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={countrySum ? colorScale(countrySum) : "#F5F4F6"}
-                />
-              );
-            })
-          }
-        </Geographies>
-      )}
-    </ComposableMap>
+      <ComposableMap
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 147
+        }}
+        height={450}
+        width={1200}
+      >
+        <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+        {data.length > 0 && (
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              // geographies is array of all the countries from geojson url
+              geographies.map(geo => {
+                // data is array of all the rows in dataset
+                const cumulative = geo.properties.NAME === "China" || geo.properties.NAME === "South Korea" || geo.properties.NAME === "Iran" || geo.properties.NAME === "Italy";
+                const countryArray = cumulative ? data.filter(d => d["Country/Region"].includes(geo.properties.NAME) && parseInt(moment(d["Last Update"]).format("DDDD")) === props.day) : data.filter(d => d["Country/Region"].includes(geo.properties.NAME) && parseInt(moment(d["Last Update"]).format("DDDD")) <= props.day); // change end to '=== props.day' to see countries with cumulative data
+                const countrySum = countryArray.reduce((acc, current, index) => acc + parseInt(countryArray[index].Confirmed), 0);
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={countrySum ? colorScale(countrySum) : "#F5F4F6"}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        )}
+      </ComposableMap>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -10,12 +10,26 @@ import MapChart from "./MapChart";
 const useStyles = makeStyles({
   root: {
     color: 'rgb(255, 108, 82)',
+  },
+  markLabel: {
+    color: 'rgb(192, 188, 196)',
   }
 });
 
+const marks = [
+  {
+    value: 33,
+    label: 'February 2, 2020',
+  },
+  {
+    value: 64,
+    label: 'March 4, 2020',
+  },
+];
+
 function App() {
   const classes = useStyles();
-  const [day, setDay] = useState(31);
+  const [day, setDay] = useState(33);
 
   const handleChange = (e, value) => {
     setDay(value); //typeof value is number
@@ -23,11 +37,16 @@ function App() {
 
   return (
     <div className="container">
+      <p className="heading">Covid-19 confirmed cases by country</p>
+      <div className="legend-container">
+        <div className="legend-box"></div>
+        <div className="legend-label">at least 5000 cases</div>
+      </div>
       <div className="map-container">
         <MapChart day={day} />
       </div>
       <div className="slider-container">
-        <Slider id="slider" onChange={handleChange} min={31} max={64} classes={{root: classes.root}} />
+        <Slider id="slider" onChange={handleChange} min={33} max={64} classes={{root: classes.root, markLabel: classes.markLabel}} marks={marks} />
       </div>
     </div>
   );
